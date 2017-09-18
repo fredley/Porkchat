bfc65e5f9a19ddc514b9890c5bc7bd481035f20b
Tom White <fredley@gmail.com>
2017-09-18 20:55:35 +0100

    Version 1.0.10 - the emojiening

--- src/porkchat.js
+++ src/porkchat.js
@@ -1,174 +1,11 @@
-//Livequery
-
-(function livequery($) {
-    /*! Copyright (c) 2010 Brandon Aaron (http://brandonaaron.net)
-     * Dual licensed under the MIT (MIT_LICENSE.txt)
-     * and GPL Version 2 (GPL_LICENSE.txt) licenses.
-     *
-     * Version: 1.1.1
-     * Requires jQuery 1.3+
-     * Docs: http://docs.jquery.com/Plugins/livequery
-     */
- 
-    $.extend($.fn, {
-        livequery: function(type, fn, fn2) {
-            var self = this, q;
-            if ($.isFunction(type))
-                fn2 = fn, fn = type, type = undefined;
-            $.each( $.livequery.queries, function(i, query) {
-                if ( self.selector == query.selector && self.context == query.context &&
-                    type == query.type && (!fn || fn.$lqguid == query.fn.$lqguid) && (!fn2 || fn2.$lqguid == query.fn2.$lqguid) )
- 
-                        return (q = query) && false;
-            });
-            q = q || new $.livequery(this.selector, this.context, type, fn, fn2);
-            q.stopped = false;
-            q.run();
-            return this;
-        },
- 
-        expire: function(type, fn, fn2) {
-            var self = this;
-            if ($.isFunction(type))
-                fn2 = fn, fn = type, type = undefined;
-            $.each( $.livequery.queries, function(i, query) {
-                if ( self.selector == query.selector && self.context == query.context &&
-                    (!type || type == query.type) && (!fn || fn.$lqguid == query.fn.$lqguid) && (!fn2 || fn2.$lqguid == query.fn2.$lqguid) && !this.stopped )
-                        $.livequery.stop(query.id);
-            });
-            return this;
-        }
-    });
- 
-    $.livequery = function(selector, context, type, fn, fn2) {
-        this.selector = selector;
-        this.context  = context;
-        this.type     = type;
-        this.fn       = fn;
-        this.fn2      = fn2;
-        this.elements = [];
-        this.stopped  = false;
-        this.id = $.livequery.queries.push(this)-1;
-        fn.$lqguid = fn.$lqguid || $.livequery.guid++;
-        if (fn2) fn2.$lqguid = fn2.$lqguid || $.livequery.guid++;
-        return this;
-    };
- 
-    $.livequery.prototype = {
-        stop: function() {
-            var query = this;
- 
-            if ( this.type )
-                this.elements.unbind(this.type, this.fn);
-            else if (this.fn2)
-                this.elements.each(function(i, el) {
-                    query.fn2.apply(el);
-                });
-            this.elements = [];
-            this.stopped = true;
-        },
- 
-        run: function() {
-            if ( this.stopped ) return;
-            var query = this;
- 
-            var oEls = this.elements,
-                els  = $(this.selector, this.context),
-                nEls = els.not(oEls);
-            this.elements = els;
- 
-            if (this.type) {
-                nEls.bind(this.type, this.fn);
-                if (oEls.length > 0)
-                    $.each(oEls, function(i, el) {
-                        if ( $.inArray(el, els) < 0 )
-                            $.event.remove(el, query.type, query.fn);
-                    });
-            }
-            else {
-                nEls.each(function() {
-                    query.fn.apply(this);
-                });
-                if ( this.fn2 && oEls.length > 0 )
-                    $.each(oEls, function(i, el) {
-                        if ( $.inArray(el, els) < 0 )
-                            query.fn2.apply(el);
-                    });
-            }
-        }
-    };
- 
-    $.extend($.livequery, {
-        guid: 0,
-        queries: [],
-        queue: [],
-        running: false,
-        timeout: null,
- 
-        checkQueue: function() {
-            if ( $.livequery.running && $.livequery.queue.length ) {
-                var length = $.livequery.queue.length;
-                while ( length-- )
-                    $.livequery.queries[ $.livequery.queue.shift() ].run();
-            }
-        },
- 
-        pause: function() {
-            $.livequery.running = false;
-        },
- 
-        play: function() {
-            $.livequery.running = true;
-            $.livequery.run();
-        },
- 
-        registerPlugin: function() {
-            $.each( arguments, function(i,n) {
-                if (!$.fn[n]) return;
- 
-                var old = $.fn[n];
- 
-                $.fn[n] = function() {
-                    var jQuery = $;
-                    var r = old.apply(this, arguments);
- 
-                    jQuery.livequery.run();
- 
-                    return r;
-                }
-            });
-        },
- 
-        run: function(id) {
-            if (id != undefined) {
-                if ( $.inArray(id, $.livequery.queue) < 0 )
-                    $.livequery.queue.push( id );
-            }
-            else
-                $.each( $.livequery.queries, function(id) {
-                    if ( $.inArray(id, $.livequery.queue) < 0 )
-                        $.livequery.queue.push( id );
-                });
- 
-            // Clear timeout if it already exists
-            if ($.livequery.timeout) clearTimeout($.livequery.timeout);
-            $.livequery.timeout = setTimeout($.livequery.checkQueue, 20);
-        },
- 
-        stop: function(id) {
-            if (id != undefined)
-                $.livequery.queries[ id ].stop();
-            else
-                $.each( $.livequery.queries, function(id) {
-                    $.livequery.queries[ id ].stop();
-                });
-        }
-    });
- 
-    $.livequery.registerPlugin('append', 'prepend', 'after', 'before', 'wrap', 'attr', 'removeAttr', 'addClass', 'removeClass', 'toggleClass', 'empty', 'remove', 'html');
- 
-    $(function() { $.livequery.play(); });
-})(jQuery)
+/*! jquery.livequery - v1.3.6 - 2016-12-09
+ * Copyright (c)
+ *  (c) 2010, Brandon Aaron (http://brandonaaron.net)
+ *  (c) 2012 - 2016, Alexander Zaytsev (https://alexzaytsev.me)
+ * Dual licensed under the MIT (MIT_LICENSE.txt)
+ * and GPL Version 2 (GPL_LICENSE.txt) licenses.
+ */
+!function(a){"function"==typeof define&&define.amd?define(["jquery"],a):a("object"==typeof exports?require("jquery"):jQuery)}(function(a,b){function c(a,b,c,d){return!(a.selector!=b.selector||a.context!=b.context||c&&c.$lqguid!=b.fn.$lqguid||d&&d.$lqguid!=b.fn2.$lqguid)}a.extend(a.fn,{livequery:function(b,e){var f,g=this;return a.each(d.queries,function(a,d){if(c(g,d,b,e))return(f=d)&&!1}),f=f||new d(g.selector,g.context,b,e),f.stopped=!1,f.run(),g},expire:function(b,e){var f=this;return a.each(d.queries,function(a,g){c(f,g,b,e)&&!f.stopped&&d.stop(g.id)}),f}});var d=a.livequery=function(b,c,e,f){var g=this;return g.selector=b,g.context=c,g.fn=e,g.fn2=f,g.elements=a([]),g.stopped=!1,g.id=d.queries.push(g)-1,e.$lqguid=e.$lqguid||d.guid++,f&&(f.$lqguid=f.$lqguid||d.guid++),g};d.prototype={stop:function(){var b=this;b.stopped||(b.fn2&&b.elements.each(b.fn2),b.elements=a([]),b.stopped=!0)},run:function(){var b=this;if(!b.stopped){var c=b.elements,d=a(b.selector,b.context),e=d.not(c),f=c.not(d);b.elements=d,e.each(b.fn),b.fn2&&f.each(b.fn2)}}},a.extend(d,{guid:0,queries:[],queue:[],running:!1,timeout:null,registered:[],checkQueue:function(){if(d.running&&d.queue.length)for(var a=d.queue.length;a--;)d.queries[d.queue.shift()].run()},pause:function(){d.running=!1},play:function(){d.running=!0,d.run()},registerPlugin:function(){a.each(arguments,function(b,c){if(a.fn[c]&&!(a.inArray(c,d.registered)>0)){var e=a.fn[c];a.fn[c]=function(){var a=e.apply(this,arguments);return d.run(),a},d.registered.push(c)}})},run:function(c){c!==b?a.inArray(c,d.queue)<0&&d.queue.push(c):a.each(d.queries,function(b){a.inArray(b,d.queue)<0&&d.queue.push(b)}),d.timeout&&clearTimeout(d.timeout),d.timeout=setTimeout(d.checkQueue,20)},stop:function(c){c!==b?d.queries[c].stop():a.each(d.queries,d.prototype.stop)}}),d.registerPlugin("append","prepend","after","before","wrap","attr","removeAttr","addClass","removeClass","toggleClass","empty","remove","html","prop","removeProp"),a(function(){d.play()})});
 
 // HamStare Shortcuts
 
@@ -322,7 +159,7 @@ var initEditButton = function() {
     button.on('click',function(){
         var text = prompt("New message text","(deleted)");
         var data = {
-            text: text, 
+            text: text,
             fkey: localStorage['pc_fkey']
         };
         $.post('//'+url_bits[2]+'/messages/'+postid, data);
@@ -698,13 +535,12 @@ function checkFriday(elem, checkAgain) {
             for(var i = 0; i < window.friday_terms.length; i++) {
                 var term = $.trim(window.friday_terms[i]);
                 if (term == ""){ continue; }
-                console.log("Checking for " + term);
                 if (page.toLowerCase().indexOf(term) > -1) {
                     elem.css('color', '#f00 !important');
                     elem.html('CAUTION: '+term.toUpperCase()+' LINK (' + elem.html() + ')');
                 } else {
                     if (checkAgain){
-                        checkFriday(elem, false); 
+                        checkFriday(elem, false);
                     }
                 }
             }
@@ -742,12 +578,19 @@ function checkStarred() {
     });
 }
 
+function checkEmoji(el){
+    if(isOnlyEmoji(el.text())){
+        el.css({'font-size':'36px', 'transition': 'font-size 0.2s'})
+    }
+}
+
 function checkMessages() {
     $('.message').each(function() {
         if (!$(this).hasClass('checked')) {
             $(this).addClass('checked');
             kindle($(this));
             checkTopic($(this));
+            checkEmoji($(this));
             if ($(this).find('.content').first().find('a').length > 0) {
                 $(this).find('.content').first().find('a').each(function() {
                     checkFriday($(this), true);
@@ -755,6 +598,13 @@ function checkMessages() {
             }
         }
     });
+}
+
+function isOnlyEmoji(str) {
+    str = str.trim()
+    // https://stackoverflow.com/a/39652525/319618
+    var r = /(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|[\ud83c[\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|[\ud83c[\ude32-\ude3a]|[\ud83c[\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff])/g
+    return str.trim().replace(r, '').length === 0
 }
 
 $(document).ready(function() {
